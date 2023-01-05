37e3df11d4720207dead16ddf1d12fa2c7257e8f
kasem667 <121999969+kasem667@users.noreply.github.com>
2023-01-06 00:25:31 +0600

    Python-programme

---
+++ some_python_programme.py
@@ -0,0 +1,13 @@
+for x in range(int(input())):
+    a,b = map(int,input().split())
+    total = []
+    if a<b:
+        for i in range(a+1,b):
+            if i%2==1:
+                total.append(i)
+        print(sum(total))
+    else:
+        for i in range(b+1,a):
+            if i%2==1:
+                total.append(i)
+        print(sum(total))
