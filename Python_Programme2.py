fa72091a56dfbb351faf46bee5117e867032ba80
kasem667 <121999969+kasem667@users.noreply.github.com>
2023-01-06 00:29:25 +0600

    Second_Python_programme
    This programme will helf you to calculate any work. All my programme are created by python.

---
+++ Python_Programme2.py
@@ -0,0 +1,9 @@
+a,b,c = list(map(float,input().split()))
+formola = ((b**2)-4*a*c)
+if a==0 or formola<0:
+    print("Impossivel calcular")
+else:
+    x1 = ((-b)+formola**0.50)/(2*a)
+    x2 = ((-b)-formola**0.50)/(2*a)
+    print(f"R1 = {x1:.5f}")
+    print(f"R2 = {x2:.5f}")
